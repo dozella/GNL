3801c0392e522e6ce489ba977b7220a8da4d6fd7
Dmitriy <103851260+dozella@users.noreply.github.com>
2022-10-03 04:58:03 +0300

    Add files via upload

---
+++ get_next_line_bonus.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dozella <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/25 17:26:57 by dozella           #+#    #+#             */
+/*   Updated: 2022/01/25 17:27:00 by dozella          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+size_t			ft_strlen(char *s);
+char			*ft_strchr(char *s, int c);
+char			*ft_strjoin(char *s1, char *s2);
+char			*get_next_line(int fd);
+char			*ft_get_line(char *rem);
+char			*ft_rem(char *rem);
+char			*ft_read(int fd, char *rem);
+
+#endif
